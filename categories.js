a247a3e77bbbf684e845f45d1f360acb082f5736
Marko Knöbl <marko.kn@gmail.com>
2016-02-24 21:10:05 +0100

    change in data structure: store categories with a list of keywords belonging to each of them

---
+++ categories.js
@@ -0,0 +1,72 @@
+"use strict";
+
+var categories = [
+	{
+		name: 'salary',
+		keywords: ['bezug']
+	},
+	{
+		name: 'groceries',
+		keywords: ['billa', 'merkur', 'penny', 'zielpunkt', 'spar dankt', 'hofer']
+	},
+	{
+		name: 'transportation',
+		keywords: ['oebb', 'wr.linien', 'db bahn']
+	},
+	{
+		name: 'transportation-car',
+		keywords: ['forstinger']
+	},
+	{
+		name: 'interest',
+		keywords: ['zinsen']
+	},
+	{
+		name: 'housing',
+		keywords: ['miete', 'wohnung', 'obi bau', 'mobelix', 'ikea', 'kika', 'xxxlutz']
+	},
+	{
+		name: 'electronics',
+		keywords: ['cyberport', 'hartlauer']
+	},
+	{
+		name: 'shopping',
+		keywords: ['amazon', 'ebay']
+	},
+	{
+		name: 'clothes',
+		keywords: ['timberland', 'green ground', 'ebenberg', 'gea', 'outlet\\\\parndorf',]
+	},
+	{
+		name: 'fuel',
+		keywords: ['omv', 'tankstelle', 'eni servicestation', 'bp']
+	},
+	{
+		name: 'books',
+		keywords: ['thalia']
+	},
+	{
+		name: 'sport',
+		keywords: ['sportsdirect', 'hervis', 'uni wien usi']
+	},
+	{
+		name: 'phone',
+		keywords: ['telekom austria']
+	},
+	{
+		name: 'atm',
+		keywords: ['auszahlung']
+	},
+	{
+		name: 'travel',
+		keywords: ['urlaub', 'flüge']
+	},
+	{
+		name: 'self',
+		keywords: ['marko', 'de giro']
+	},
+	{
+		name: 'taxes',
+		keywords: ['sozialvers']
+	}
+];
