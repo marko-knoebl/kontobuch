bfd561eeaf2e85b6ab4f7b2e18bac620b91e8432
Marko Knöbl <marko.kn@gmail.com>
2016-03-28 17:32:54 +0200

    rework the "create account" dialog: make fullscreen on smaller devices, add X button

--- myfinances-angular.js
+++ myfinances-angular.js
@@ -10,55 +10,26 @@ myFinancesModule.controller('MyFinancesCtrl', function($scope, $mdDialog, $mdMed
   $scope.currentBalance = 0;
   $scope.csvImportConfig = konto.csvImportConfig;
   $scope.showNewAccountDialog = function(event, bank) {
-    var controller;
-    if (bank === 'bawagpsk') {
-      controller = NewBawagpskAccountDialogController;
-    } else if (bank === 'hellobank') {
-      controller = NewHellobankAccountDialogController;
-    } else if (bank === 'raiffeisen') {
-      controller = NewRaiffeisenAccountDialogController;
-    } else if (bank === 'paypal') {
-      controller = NewPaypalAccountDialogController;
-    }
+    // create a new controller corresponding to the selected bank
+    var controller = function($scope, $mdDialog) {
+      $scope.cancel = $mdDialog.cancel;
+      $scope.processOKClicked = function() {
+        data.currentBalance = $scope.currentBalance;
+        //$scope.$parent.currentBalance = data.currentBalance;
+        readCSVandUpdateChart(bank, $mdDialog.hide);
+      };
+    };
     $mdDialog.show({
       templateUrl: 'template-new-account-' + bank + '.html',
       clickOutsideToClose: true,
       controller: controller,
       // create a new child scope of the global angular scope
       scope: $scope.$new(),
-      targetEvent: event
+      targetEvent: event,
+      fullscreen: true
     });
   };
 });
-
-function NewBawagpskAccountDialogController($scope, $mdDialog) {
-  $scope.processOKClicked = function() {
-    data.currentBalance = $scope.currentBalance;
-    //$scope.$parent.currentBalance = data.currentBalance;
-    readCSVandUpdateChart('bawagpsk', $mdDialog.hide);
-  };
-}
-
-function NewHellobankAccountDialogController($scope, $mdDialog) {
-  $scope.processOKClicked = function() {
-    data.currentBalance = $scope.currentBalance;
-    readCSVandUpdateChart('hellobank', $mdDialog.hide);
-  };
-}
-
-function NewRaiffeisenAccountDialogController($scope, $mdDialog) {
-  $scope.processOKClicked = function() {
-    data.currentBalance = $scope.currentBalance;
-    readCSVandUpdateChart('raiffeisen', $mdDialog.hide);
-  };
-}
-
-function NewPaypalAccountDialogController($scope, $mdDialog) {
-  $scope.processOKClicked = function() {
-    data.currentBalance = $scope.currentBalance;
-    readCSVandUpdateChart('paypal', $mdDialog.hide);
-  };
-}
 
 var copyTransactionsToAngularScope;
 
